934ef027664152c54ddb611c7733ef16810e4c4c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-07 19:32:33 -0300

    Exercicio quarta vez

---
+++ Reactjs/Chapter-1/4-quarta/src/components/RepositoryItem.tsx
@@ -0,0 +1,17 @@
+interface props {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
+
+export function RepositoryItem({ repository }: props) {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+      <a href={repository.html_url}>Acessar repositorios</a>
+    </li>
+  );
+}
