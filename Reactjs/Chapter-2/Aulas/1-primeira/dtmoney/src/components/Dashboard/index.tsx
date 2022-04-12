845ebf5732803a4d8b104c7975a2f3aa03c4e3b8
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-12 11:29:59 -0300

    Layout Dashboard

--- Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/components/Dashboard/index.tsx
+++ Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/components/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Summary from "../Summary";
+import TransactionTable from "../TransactionTable";
 
 import { Container } from "./styles";
 
@@ -7,6 +8,7 @@ const Dashboard: React.FC = () => {
   return (
     <Container>
       <Summary />
+      <TransactionTable />
     </Container>
   );
 };
---
+++ Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/components/TransactionTable/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const TransactionTable: React.FC = () => {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit"> R$ 7.000,00</td>
+            <td>Salario</td>
+            <td>05/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 2.000,00</td>
+            <td>Casa</td>
+            <td>15/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
+
+export default TransactionTable;
