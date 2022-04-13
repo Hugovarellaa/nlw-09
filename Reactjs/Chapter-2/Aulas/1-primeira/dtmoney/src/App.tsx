d4c80d7e127bc531147ae05be63a1cd71e340d32
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-13 16:46:37 -0300

    Table transactions

--- Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/App.tsx
+++ Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyles />
     </>
   );
