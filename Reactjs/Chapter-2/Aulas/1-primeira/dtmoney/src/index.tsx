1d5f69fad2a25910a4d80f26cf320b21aa377f0c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-13 17:20:21 -0300

    Configurando o miragejs e o axios

--- Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/index.tsx
+++ Reactjs/Chapter-2/Aulas/1-primeira/dtmoney/src/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "teste 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
 root.render(
   <React.StrictMode>
     <App />
