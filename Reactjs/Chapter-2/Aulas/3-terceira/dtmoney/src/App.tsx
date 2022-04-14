b44d62220e2909879cbac60e0d7e40a136d5e9e8
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-14 17:27:33 -0300

    estrutura configurada

---
+++ Reactjs/Chapter-2/Aulas/3-terceira/dtmoney/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Testando</h1>
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
