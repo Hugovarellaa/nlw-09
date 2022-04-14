d5dd18c101a5948c361a77da1f115bd91f0feaa8
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-14 11:03:27 -0300

    Layout Transactions

---
+++ Reactjs/Chapter-2/Aulas/2-segunda/dtmoney/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
