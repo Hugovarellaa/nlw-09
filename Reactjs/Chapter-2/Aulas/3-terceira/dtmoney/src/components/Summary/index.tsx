87b0af3cbc8378cf46f538839e8a4845fdfd7b17
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-14 17:52:24 -0300

    Layout Summary

---
+++ Reactjs/Chapter-2/Aulas/3-terceira/dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+const Summary: React.FC = () => {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$ 12.000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+        <strong>- R$ 7.000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$ 5.000,00</strong>
+      </div>
+    </Container>
+  );
+};
+
+export default Summary;
