5cba80852c7d509428d9404ccf4c88221edcd931
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-17 16:04:50 -0300

    Layout header

---
+++ Reactjs/Chapter-2/Aulas/5-quinta/dtmoney/src/styles/GlobalStyles.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root{
+        --background: #f0f2f5;
+        --red: #E52E4D;
+        --blue: #5429cc;
+        --blue-light: #6933ff;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        --shape:#FFFFFF;
+        --green: #33CC95;
+    }
+
+    @media(max-width:1080px){
+      html{
+        font-size:93.75%;
+      }
+    }
+    @media(max-width:720px){
+      html{
+        font-size: 87.5%;
+      }
+    }
+
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      background: var(--background);
+      -webkit-font-smoothing: antialiased;
+    }
+
+    body, select, button, input, textarea {
+      font-family: "Poppins" , sans-serif;
+      font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+      font-weight: 600;
+    }
+
+    button {
+      cursor: pointer
+    }
+
+    a{
+      text-decoration: none
+    }
+
+    [disabled]{
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+`;
