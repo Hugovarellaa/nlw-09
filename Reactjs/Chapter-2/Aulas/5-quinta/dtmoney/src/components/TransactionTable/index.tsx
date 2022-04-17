81fbd44808cf276468a3ab22aa2e192a3c6161d0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-17 16:21:07 -0300

    Layout TransactionTable

--- Reactjs/Chapter-2/Aulas/5-quinta/dtmoney/src/components/TransactionTable/index.tsx
+++ Reactjs/Chapter-2/Aulas/5-quinta/dtmoney/src/components/TransactionTable/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const TransactionTable: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Salario</td>
+            <td>05/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 2.000,00</td>
+            <td>Casa</td>
+            <td>22/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 };
 
 export default TransactionTable;
