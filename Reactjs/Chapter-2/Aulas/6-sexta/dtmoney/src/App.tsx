a386fc280caae87da35f8b95c56cb2e8694255ba
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-18 10:13:33 -0300

    Layout Dashboard

--- Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/App.tsx
+++ Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Dashboard from "./components/Dashboard";
 import Header from "./components/Header";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
@@ -6,6 +7,7 @@ const App: React.FC = () => {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyles />
     </>
   );
---
+++ Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/components/TransactionTable/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const TransactionTable: React.FC = () => {
+  return <Container>{/* <h1>transaction table</h1> */}</Container>;
+};
+
+export default TransactionTable;
