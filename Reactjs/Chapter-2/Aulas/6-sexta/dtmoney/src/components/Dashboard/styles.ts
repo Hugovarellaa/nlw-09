fb7d28247159e4d582ace53b4f38de2ad816382d
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-18 10:21:38 -0300

    Layout TransactionTable

--- Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/components/Dashboard/styles.ts
+++ Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/components/Dashboard/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.main`
   max-width: 1120px;
   margin: 0 auto;
   padding: 1rem 2rem;
--- Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/components/TransactionTable/index.tsx
+++ Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/components/TransactionTable/index.tsx
@@ -3,7 +3,35 @@ import React from "react";
 import { Container } from "./styles";
 
 const TransactionTable: React.FC = () => {
-  return <Container>{/* <h1>transaction table</h1> */}</Container>;
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Salario</td>
+            <td>22/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 2.000,00</td>
+            <td>Casa</td>
+            <td>30/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 };
 
 export default TransactionTable;
