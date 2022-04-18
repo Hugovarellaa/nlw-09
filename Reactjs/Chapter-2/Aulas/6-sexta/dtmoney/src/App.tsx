4c4bbb634dccdd3cc3e61a0e0e8a239aa5c24ac2
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-18 10:01:54 -0300

    Layout Header

---
+++ Reactjs/Chapter-2/Aulas/6-sexta/dtmoney/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
