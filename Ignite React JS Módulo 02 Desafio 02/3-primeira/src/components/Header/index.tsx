3a571cea3524007064c6615f41e0d4a00bdbe7dc
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-25 17:57:49 -0300

    Components Header convertido para function compoenents

---
+++ Ignite React JS Módulo 02 Desafio 02/3-primeira/src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { FiPlusSquare } from "react-icons/fi";
+
+import { Container } from "./styles";
+import Logo from "../../assets/logo.svg";
+
+interface HeaderProops {
+  openModal: () => void;
+}
+
+export default function Header({ openModal }: HeaderProops) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button type="button" onClick={openModal}>
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
+}
