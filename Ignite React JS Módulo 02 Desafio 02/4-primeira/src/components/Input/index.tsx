b59c751b62248eda7f13e34a0d7041f155faef2a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-26 08:14:46 -0300

    Convertendo class components para function compoenents - components/Input

--- Ignite React JS Módulo 02 Desafio 02/4-primeira/src/components/Input/index.tsx
+++ Ignite React JS Módulo 02 Desafio 02/4-primeira/src/components/Input/index.tsx
@@ -1,19 +1,20 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+import { IconBaseProps } from "react-icons";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InpputProps {
+  name: string;
+  placeholder: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
 
+export default function Input({ name, icon: Icon, ...rest }: InpputProps) {
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const { fieldName, defaultValue, registerField } = useField(name);
 
@@ -31,7 +32,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -48,6 +49,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
