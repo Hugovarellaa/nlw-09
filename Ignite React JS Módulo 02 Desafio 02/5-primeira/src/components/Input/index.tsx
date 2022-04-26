0d46ce9cc252250a1bd99c3d73aebce59b685e0b
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-04-26 09:29:56 -0300

    Class components para Function compoennts - components/Input

--- Ignite React JS Módulo 02 Desafio 02/5-primeira/src/components/Input/index.tsx
+++ Ignite React JS Módulo 02 Desafio 02/5-primeira/src/components/Input/index.tsx
@@ -8,9 +8,16 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconBaseProps } from 'react-icons/lib';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder: string;
+  icon: React.ComponentType<IconBaseProps>
+}
+
+export default function Input({ name, icon: Icon, ...rest }: InputProps){
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -35,6 +42,8 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
 
+ 
+
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
@@ -48,6 +57,5 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
-export default Input;
