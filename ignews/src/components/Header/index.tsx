7fa52e087a5686949df072c28508863aec6ab958
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-09 17:32:16 -0300

    Layout Header

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import styles from "./styles.module.scss";
+
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/logo.svg" alt="Logo Ignews" />
+        <nav>
+          <a href="/">Home</a>
+          <a href="/posts" className={styles.active}>
+            Post
+          </a>
+        </nav>
+        {/* botao de login */}
+      </div>
+    </header>
+  );
+}
