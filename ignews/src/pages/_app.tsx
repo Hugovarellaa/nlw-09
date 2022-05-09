187b28fe9bdf20906806fa66c12685289aace594
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-09 10:19:20 -0300

    Layout index

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import type { AppProps } from 'next/app'
-import "../styles/global.scss"
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
+
 import Head from "next/head";
-import Header from "../components/Header";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +9,22 @@ export default function Home() {
       <Head>
         <title>Ignews | Home</title>
       </Head>
-      <Header />
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications
+            <br />
+            <span>For $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
