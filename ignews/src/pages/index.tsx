f75dab1e22af9f8bb1345251f9ff65f1f27f9513
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-09 10:42:01 -0300

    Chamada API stripe

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -20,11 +30,29 @@ export default function Home() {
           <p>
             Get acess to all the publications
             <br />
-            <span>For $9.90 month</span>
+            <span>For {product.amount} month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prices = await stripe.prices.retrieve("price_1JsSzEH6aihmDxYbnis4Pjbe");
+
+  const product = {
+    priceId: prices.id,
+    amount: new Intl.NumberFormat("en", {
+      style: "currency",
+      currency: "USD",
+    }).format(prices.unit_amount / 100),
+  };
+  console.log(product);
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, //24hours
+  };
+};
---
+++ ignews/src/services/stripe.ts
@@ -0,0 +1,9 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
+  apiVersion: "2020-08-27",
+  appInfo: {
+    name: "Ignews",
+    version: "^8.222.0",
+  },
+});
