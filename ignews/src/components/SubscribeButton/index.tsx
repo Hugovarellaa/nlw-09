a0b06e8a7db139a150d370527f80ceecf0dcfd64
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-09 15:42:36 -0300

    Delete ignews directory

--- ignews/src/components/SubscribeButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss";
-
-export function SubscribeButton() {
-  return (
-    <button type="button" className={styles.subscribeButton}>
-      Subscribe now
-    </button>
-  );
-}
