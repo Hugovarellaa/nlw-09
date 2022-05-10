49b2cc2d6142646b8d774976d73a1b14ed0d8104
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-10 11:06:56 -0300

    Tratando eventos no Stripe e salvando no banco de dados do faunaDB

---
+++ ignews/src/pages/api/_lib/managerSubscription.ts
@@ -0,0 +1,32 @@
+import { fauna } from "../../../services/faunadb";
+import { query as q } from "faunadb";
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscriptions(
+  subscriptionId: string,
+  customerId: string
+) {
+  // Buscar o usuário no banco do FaunaDB com o ID {customerId}
+  // Salvar os dados da subscription no FaundaDB
+
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  await fauna.query(
+    q.Create(q.Collection("subscriptions"), {
+      data: subscriptionData,
+    })
+  );
+}
