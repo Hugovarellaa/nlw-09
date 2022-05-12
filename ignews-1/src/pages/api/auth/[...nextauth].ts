2919726fc6752a70d585ed44b75074802f49acc5
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-12 09:08:10 -0300

    Salvando login da API next-auth no banco de dados faunaDB

--- ignews-1/src/pages/api/auth/[...nextauth].ts
+++ ignews-1/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +12,28 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        console.log({ error: error.message });
+        return false;
+      }
+    },
+  },
 });
