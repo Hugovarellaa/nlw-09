dcf4ee85eab800376647b9a14bc622863a43c60b
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-12 09:44:26 -0300

    Conectando o faunadb ao Stripe e salvando dados no banco evitando duplicidade de dados

--- ignews-1/src/components/SubscribeButton/index.tsx
+++ ignews-1/src/components/SubscribeButton/index.tsx
@@ -1,9 +1,33 @@
-import styles from "./styles.module.scss"
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../pages/api/api";
+import { getStripeJs } from "../../services/stripejs";
+import styles from "./styles.module.scss";
 
-export function SubscriberButton (){
+export function SubscriberButton() {
+  const { data: session } = useSession();
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert({ error: error.message });
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscriberButton}>
+    <button
+      type="button"
+      className={styles.subscriberButton}
+      onClick={handleSubscribe}
+    >
       Subscriber now
     </button>
-  )
+  );
 }
