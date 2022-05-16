e74bb4a3a64c1be1c46e5e8537be62ed94679c4d
Hugo <hugovarellaa@gmail.com>
2022-05-16 17:49:08 -0300

    Criando rota de checkout session no stripe no front-end

--- ignews-1/ignews/src/components/SubscribeButton/index.tsx
+++ ignews-1/ignews/src/components/SubscribeButton/index.tsx
@@ -1,15 +1,22 @@
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/axios";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 export function SubscribeButton() {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
     //checkoutSession stripe
+    const response = await api.post("/subscribe");
+
+    const { sessionId } = response.data;
+    const stripe = await getStripeJs();
+    stripe.redirectToCheckout({ sessionId });
   }
 
   return (
