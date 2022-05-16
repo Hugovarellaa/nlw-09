728a1c18c3a71d43ba5cc6028a8a9caa93fe587e
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-16 12:03:07 -0300

    Delete ignews-1 directory

--- ignews-1/src/services/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from "stripe";
-
-export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
-  apiVersion: "2020-08-27",
-  appInfo: {
-    name: "Ignews-1",
-    version: "^9.1.0",
-  },
-});
--- ignews-1/src/services/stripejs.ts
+++
@@ -1,6 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-export function getStripeJs() {
-  const stripeJs = loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
-  return stripeJs;
-}
