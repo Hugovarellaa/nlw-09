9b7244569273b3c8d73dc249f1c82af10658f21c
Hugo <hugovarellaa@gmail.com>
2022-05-16 16:15:34 -0300

    Chamada API stripe no index.tsx ( Home )

--- ignews-1/ignews/src/pages/index.tsx
+++ ignews-1/ignews/src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function App() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+}
+
+export default function App({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,7 +27,7 @@ export default function App() {
           </h1>
           <p>
             Get access to all the publications <br />{" "}
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -27,3 +36,19 @@ export default function App() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JsSzEH6aihmDxYbnis4Pjbe");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: { product },
+  };
+};
