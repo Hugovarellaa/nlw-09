01a6879a416e219964cd581fec9beee4d68af44c
Hugo <hugovarellaa@gmail.com>
2022-05-16 21:08:22 -0300

    Ouvindo  Webhook de checout-session-completed do stripe

---
+++ ignews-1/ignews/src/pages/api/_lib/managerSubription.ts
@@ -0,0 +1,36 @@
+import { fauna } from "../../../services/faunadb";
+import { query as q } from "faunadb";
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscriptions(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false
+) {
+  // Buscar o usuario no banco do faunadb com o ID customerId (stripe_customer_id)
+  // Salvar os dados da subscription do usuario no banco faunaDB (subscriptions)
+
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(q.Match(q.Index("user_by_stripe_customer_id"), customerId))
+    )
+  );
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userRef: userRef,
+    status: subscription.status,
+    priceId: subscription.items.data[0].price.id,
+  };
+
+  if (createAction) {
+    await fauna.query(
+      q.Create(q.Collection("subscriptions"), {
+        data: subscriptionData,
+      })
+    );
+  } else {
+  }
+}
