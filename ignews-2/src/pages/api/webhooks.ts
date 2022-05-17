3571e339da9fbf46b376a04558f47c6642c23875
Hugo <hugovarellaa@gmail.com>
2022-05-17 17:26:46 -0300

    Tratando Eventos

--- ignews-2/src/pages/api/webhooks.ts
+++ ignews-2/src/pages/api/webhooks.ts
@@ -1,9 +1,61 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhook = process.env.STRIPE_WEBHOOK_SECRET;
+
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    console.log("evento recebido");
+    const buf = await buffer(req);
+    const secret = req.headers["stripe-signature"];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, webhook);
+    } catch (error) {
+      res.status(400).end(`Webhook error ${error.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            break;
+          case "checkout.session.completed":
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (error) {
+        
+      }
+    }
 
     res.json({ received: true });
   } else {
