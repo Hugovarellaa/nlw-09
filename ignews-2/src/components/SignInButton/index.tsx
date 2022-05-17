60327ba0ca3c6edfcccdc058531b4987122edaef
Hugo <hugovarellaa@gmail.com>
2022-05-17 12:03:12 -0300

    Update botao de login github estrutura

--- ignews-2/src/components/SignInButton/index.tsx
+++ ignews-2/src/components/SignInButton/index.tsx
@@ -1,8 +1,19 @@
-import styles from "./styles.module.scss"
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
-export function SignInButton (){
-  return (
+export function SignInButton() {
+  const isLog = true
+
+  return isLog ? (
     <button type="button" className={styles.signInButton}>
+      <FaGithub color="var(--green-500)"/>
+      Hugo Alves Varella
+      <FiX className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="var(--yellow-500)"/>
       Sign In with Github
     </button>
   )
