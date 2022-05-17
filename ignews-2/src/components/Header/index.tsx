8d320b09b32949a5f4962b81e7ed9fd7466362d7
Hugo <hugovarellaa@gmail.com>
2022-05-17 11:56:05 -0300

    Estilo menu Header

--- ignews-2/src/components/Header/index.tsx
+++ ignews-2/src/components/Header/index.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo Ignews" />
         <nav>
           <Link href="/">
-            <a>Home</a>
+            <a className={styles.active}>Home</a>
           </Link>
           <Link href="/posts">
             <a>Posts</a>
