96e1dd1efdd2689f80b1d5ddd879e189dc31292f
Hugo <hugovarellaa@gmail.com>
2022-05-17 15:43:22 -0300

    Fazendo login com Github

--- ignews-2/src/components/SignInButton/index.tsx
+++ ignews-2/src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isLog = true
+  const { data: session } = useSession();
 
-  return isLog ? (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="var(--green-500)"/>
-      Hugo Alves Varella
-      <FiX className={styles.closeIcon}/>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="var(--green-500)" />
+      {session.user.name}
+      <FiX className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="var(--yellow-500)"/>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="var(--yellow-500)" />
       Sign In with Github
     </button>
-  )
+  );
 }
--- ignews-2/src/pages/_app.tsx
+++ ignews-2/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import "../styles/global.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Header/>
       <Component {...pageProps} />
     </SessionProvider>
   );
