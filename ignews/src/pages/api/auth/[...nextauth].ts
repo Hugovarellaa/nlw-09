8e395f5c2bb945bb5478fa06bad17fe12ab34740
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 <hugovarellaa@gmail.com>
2022-05-18 11:07:16 -0300

    Começando a configurar botão de inscrição

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,8 +13,28 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
+    async signIn({ user, account, profile }) {
+      try {
+        const { email } = user;
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.log({ err: err.message });
+        return false;
+      }
     },
   },
 });
