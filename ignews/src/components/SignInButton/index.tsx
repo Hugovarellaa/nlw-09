22a7556ed0c50c3c5bbe358613d156e26b8f8c84
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 <hugovarellaa@gmail.com>
2022-05-18 09:34:47 -0300

    Estrutura do botao de login

---
+++ ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss"
+
+export function SigninButton() {
+  const isLog = true;
+
+  return isLog ? (
+    <button type="button">
+      <FaGithub color="var(--green-500)" />
+      Hugo Alves Varella
+      <FiX className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button">
+      <FaGithub color="var(--yellow-500)" />
+      Sign in with Github
+    </button>
+  );
+}
