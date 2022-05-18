35c061f0a1191eb38d3ce4893ecceddfedea16e6
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 <hugovarellaa@gmail.com>
2022-05-18 09:39:18 -0300

    Estilos botao de sign in github

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -6,13 +6,13 @@ export function SigninButton() {
   const isLog = true;
 
   return isLog ? (
-    <button type="button">
+    <button type="button" className={styles.signinButton}>
       <FaGithub color="var(--green-500)" />
       Hugo Alves Varella
       <FiX className={styles.closeIcon}/>
     </button>
   ) : (
-    <button type="button">
+    <button type="button" className={styles.signinButton}>
       <FaGithub color="var(--yellow-500)" />
       Sign in with Github
     </button>
