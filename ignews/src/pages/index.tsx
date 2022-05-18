f0928a81463929a09a771f5cdbc324a5e429ee79
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 <hugovarellaa@gmail.com>
2022-05-18 10:30:30 -0300

    Chamada API stripe via SSG

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -4,7 +4,14 @@ import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,7 +28,7 @@ export default function Home() {
           <p>
             Get acess to all the publications
             <br />
-            <span> for $9,90 month</span>
+            <span> for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -34,8 +41,17 @@ export default function Home() {
 export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JsSzEH6aihmDxYbnis4Pjbe");
 
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
 
   return {
-    props: {},
+    props: {
+      product,
+    },
   };
 };
