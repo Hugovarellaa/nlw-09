b8723324902a561c3f7666f885d8db36b401b715
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 <hugovarellaa@gmail.com>
2022-05-18 11:18:09 -0300

    Configurando Back-end do stripe

---
+++ ignews/src/pages/api/subscribe.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { stripe } from "../../services/stripe";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price: "price_1JsSzEH6aihmDxYbnis4Pjbe",
+          quantity: 1,
+        },
+      ],
+      allow_promotion_codes: true,
+      mode: "subscription",
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return res.json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(400).end("Method not allowed");
+  }
+};
