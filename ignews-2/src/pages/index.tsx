cd0785529c9a54c5c79a33b6caa7eae5ac25916f
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 <hugovarellaa@gmail.com>
2022-05-19 08:20:44 -0300

    _documents + fonts

--- ignews-2/src/pages/index.tsx
+++ ignews-2/src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head"
+
 export default function Home (){
   return (
+    <>
+    <Head>
+      <title>Home | Ignews</title>
+    </Head>
     <h1>testando</h1>
+    
+    </>
   )
 }
