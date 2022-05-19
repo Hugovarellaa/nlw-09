fa0f065fdadf6516b0cfd87e29714e84b6208360
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 <hugovarellaa@gmail.com>
2022-05-19 08:57:31 -0300

    Botão de login finalizado

--- ignews-2/src/components/SignInButton/index.tsx
+++ ignews-2/src/components/SignInButton/index.tsx
@@ -3,9 +3,17 @@ import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 export function SigninButton() {
-  return (
+  const isLog = false;
+
+  return isLog ? (
     <button type="button" onClick={() => {}} className={styles.signinButton}>
-      <FaGithub />
+      <FaGithub color="var(--green-500)" />
+      Hugo Alves Varella
+      <FiX className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" onClick={() => {}} className={styles.signinButton}>
+      <FaGithub color="var(--yellow-500)" />
       Sign in with Github
     </button>
   );
