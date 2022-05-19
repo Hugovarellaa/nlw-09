78c2aa241a1c4ef72f368b622b5a58d1fca27fe8
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 <hugovarellaa@gmail.com>
2022-05-19 09:01:18 -0300

    HomePage estrutura

--- ignews-2/src/pages/index.tsx
+++ ignews-2/src/pages/index.tsx
@@ -1,13 +1,26 @@
-import Head from "next/head"
+import Head from "next/head";
 
-export default function Home (){
+export default function Home() {
   return (
     <>
-    <Head>
-      <title>Home | Ignews</title>
-    </Head>
-    <h1>testando</h1>
-    
+      <Head>
+        <title>Home | Ignews</title>
+      </Head>
+
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
-  )
+  );
 }
