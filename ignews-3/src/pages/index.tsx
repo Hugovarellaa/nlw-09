e6c2a55b1ba249555d60934d457082a268c2cbbb
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 <hugovarellaa@gmail.com>
2022-05-20 07:33:05 -0300

    Home Page

--- ignews-3/src/pages/index.tsx
+++ ignews-3/src/pages/index.tsx
@@ -1,5 +1,27 @@
-export default function Home (){
+import Head from "next/head";
+import styles from "./home.module.scss";
+
+export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications
+            <br /> <span>for $9,90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
