2a1129e3499e61f3d11b549025aabf675ca05dcc
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 <hugovarellaa@gmail.com>
2022-05-20 17:24:26 -0300

    Redirecionamento e permisoes

--- ignews-3/src/pages/posts/[slug].tsx
+++ ignews-3/src/pages/posts/[slug].tsx
@@ -39,7 +39,17 @@ export const getServerSideProps: GetServerSideProps = async ({
   req,
   params,
 }) => {
-  const session = getSession({ req });
+  const session = await getSession({ req });
+
+  if (!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   const { slug } = params;
 
   const prismic = getPrismicClient(req);
