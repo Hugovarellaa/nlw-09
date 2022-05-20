aa0defbc44441a20093d0c0de90cdc7c6bd07406
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 <hugovarellaa@gmail.com>
2022-05-20 15:22:25 -0300

    Estrutura da pagina Posts

---
+++ ignews-3/src/pages/posts/index.tsx
@@ -0,0 +1,59 @@
+import Head from "next/head";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main>
+        <div>
+          <a href="">
+            <time>02 de janeiro de 2021</time>
+            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Laudantium commodi, sapiente maiores blanditiis distinctio illo
+              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
+              accusantium, soluta veniam magni suscipit molestiae facere fugit.
+            </p>
+          </a>
+
+          <a href="">
+            <time>02 de janeiro de 2021</time>
+            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Laudantium commodi, sapiente maiores blanditiis distinctio illo
+              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
+              accusantium, soluta veniam magni suscipit molestiae facere fugit.
+            </p>
+          </a>
+
+          <a href="">
+            <time>02 de janeiro de 2021</time>
+            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Laudantium commodi, sapiente maiores blanditiis distinctio illo
+              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
+              accusantium, soluta veniam magni suscipit molestiae facere fugit.
+            </p>
+          </a>
+
+          <a href="">
+            <time>02 de janeiro de 2021</time>
+            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Laudantium commodi, sapiente maiores blanditiis distinctio illo
+              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
+              accusantium, soluta veniam magni suscipit molestiae facere fugit.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
