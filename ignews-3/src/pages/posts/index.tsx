78d866bf05222848340e428c4045f49bcc08c635
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 <hugovarellaa@gmail.com>
2022-05-20 16:03:51 -0300

    Listando os posts na tela

--- ignews-3/src/pages/posts/index.tsx
+++ ignews-3/src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import Link from "next/link";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,51 +26,51 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.content}>
-          <a href="">
-            <time>02 de janeiro de 2021</time>
-            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Laudantium commodi, sapiente maiores blanditiis distinctio illo
-              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
-              accusantium, soluta veniam magni suscipit molestiae facere fugit.
-            </p>
-          </a>
-
-          <a href="">
-            <time>02 de janeiro de 2021</time>
-            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Laudantium commodi, sapiente maiores blanditiis distinctio illo
-              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
-              accusantium, soluta veniam magni suscipit molestiae facere fugit.
-            </p>
-          </a>
-
-          <a href="">
-            <time>02 de janeiro de 2021</time>
-            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Laudantium commodi, sapiente maiores blanditiis distinctio illo
-              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
-              accusantium, soluta veniam magni suscipit molestiae facere fugit.
-            </p>
-          </a>
-
-          <a href="">
-            <time>02 de janeiro de 2021</time>
-            <strong>Alguma coisa esta acontecendo enquanto eu estudo</strong>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Laudantium commodi, sapiente maiores blanditiis distinctio illo
-              delectus quas mollitia ipsam! Distinctio necessitatibus explicabo
-              accusantium, soluta veniam magni suscipit molestiae facere fugit.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query<any>(
+    [Prismic.predicates.at("document.type", "teste")],
+    {
+      fetch: ["teste.title", "teste.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
