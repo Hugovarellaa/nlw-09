559094fcb363e11f87795adf99114afdca2cbd49
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 <hugovarellaa@gmail.com>
2022-05-20 07:46:42 -0300

    Botao de inscrição

---
+++ ignews-3/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  return (
+    <button type="button" onClick={() => {}} className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
