2097e53a207dfdc87e77c6d96978e64ca5671fd2
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 <hugovarellaa@gmail.com>
2022-05-22 10:21:19 -0300

    Salvando email de login no FaunaDb

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,13 +1,29 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export function SignInButton() {
-  return (
-    <button type="button" className={styles.signInButton}>
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="var(--green-500)" />
-      hugo alves varella
-      <FiX className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="var(--yellow-500)" />
+      Sign in with Github
     </button>
   );
 }
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +17,21 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          ),
+          q.Create(q.Collection("users"), {
+            data: { email },
+          }),
+          q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+        )
+      );
+      return true;
+    },
+  },
 });
