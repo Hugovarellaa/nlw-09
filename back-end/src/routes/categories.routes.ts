4ea9b50cb499a62ac24b39d92e28e7b68407405f
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 <hugovarellaa@gmail.com>
2022-06-13 15:10:03 -0300

    Upload de arquivo pelo insomnia

--- back-end/src/routes/categories.routes.ts
+++ back-end/src/routes/categories.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/UseCases/CreateCategory";
 import { listCategoriesController } from "../modules/cars/UseCases/ListCategories";
 
 const categoriesRouter = Router();
+const upload = multer({ dest: "./tmp" });
 
 categoriesRouter.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -11,6 +13,12 @@ categoriesRouter.post("/", (request, response) => {
 
 categoriesRouter.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRouter.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  response.send();
 });
 
 export { categoriesRouter };
