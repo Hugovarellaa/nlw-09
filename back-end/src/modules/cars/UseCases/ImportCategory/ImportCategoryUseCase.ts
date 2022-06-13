d858fba84b8dda13c63fcbfc9734800cf69659ff
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 <hugovarellaa@gmail.com>
2022-06-13 16:02:34 -0300

    CSV-Parse File

--- back-end/src/modules/cars/UseCases/ImportCategory/ImportCategoryUseCase.ts
+++ back-end/src/modules/cars/UseCases/ImportCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,11 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+    const parseFile = parse();
+    stream.pipe(parseFile);
   }
 }
 
