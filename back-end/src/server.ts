9b224b702299d24c968607276dcbb4d82b3acdc8
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 <hugovarellaa@gmail.com>
2022-06-13 11:33:46 -0300

    Specification

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { categoriesRouter } from "./routes/categories.routes";
+import { specificationsRouter } from "./routes/specification.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/categories", categoriesRouter);
+app.use("/specification", specificationsRouter);
 
 app.listen(3333, () => console.log("Server started on port 3333"));
