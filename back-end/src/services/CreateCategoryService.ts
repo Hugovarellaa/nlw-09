9560c1db629527861d5517719ccb6e11af54d67b
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 <hugovarellaa@gmail.com>
2022-06-13 10:36:41 -0300

    CreateCategoryService

--- back-end/src/services/CreateCategoryService.ts
+++ back-end/src/services/CreateCategoryService.ts
@@ -1,3 +1,20 @@
-class CreateCategoryService {}
+import { CategoriesRepositories } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoryRepository: CategoriesRepositories) {}
+  execute({ name, description }: IRequest): void {
+    const categoriesAlreadyExist = this.categoryRepository.findByName(name);
+    if (categoriesAlreadyExist) {
+      throw new Error("Category already exists");
+    }
+
+    this.categoryRepository.create({ name, description });
+  }
+}
 
 export { CreateCategoryService };
