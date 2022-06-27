602720777d5f1b8c3c93e6a14123167c087aa687
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 <hugovarellaa@gmail.com>
2022-06-27 19:11:25 -0300

    Stage 2

--- nlw-06-togget-origin/main.js
+++ nlw-06-togget-origin/main.js
@@ -11,6 +11,20 @@ for (const link of links) {
 function openMenu() {
   nav.classList.add("show");
 }
+
 function closeMenu() {
   nav.classList.remove("show");
 }
+
+// shadow header with  scroll
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
+  if (window.scrollY >= navHeight) {
+    //scroll e maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    //scroll e menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
