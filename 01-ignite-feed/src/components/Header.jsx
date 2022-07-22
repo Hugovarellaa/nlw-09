c659508d1d74843b9eda62d700b125deeca1d290
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 <hugovarellaa@gmail.com>
2022-07-22 19:14:28 -0300

    Header finalizado

--- 01-ignite-feed/src/components/Header.jsx
+++ 01-ignite-feed/src/components/Header.jsx
@@ -1,9 +1,11 @@
+import logoImg from '../assets/logo.svg'
 import styles from './Header.module.css'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
-      <h1>ola</h1>
+      <img src={logoImg} alt="" />
+      <span>Ignite Feed</span>
     </header>
   )
 }
