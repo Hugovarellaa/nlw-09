d0d21649965e1a78b22934396d0361b903db1b95
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 <hugovarellaa@gmail.com>
2022-07-22 18:02:24 -0300

    Header components

--- 01-ignite-feed/src/App.jsx
+++ 01-ignite-feed/src/App.jsx
@@ -1,3 +1,9 @@
+import { Header } from './components/Header'
+
 export function App() {
-  return <h1>ola mundo</h1>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
---
+++ 01-ignite-feed/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <h1>ola</h1>
+    </header>
+  )
+}
