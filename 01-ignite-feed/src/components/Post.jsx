50a1ac68ddc463319d473276708ca71298855248
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 <hugovarellaa@gmail.com>
2022-07-22 21:34:13 -0300

    Botao de publicar

--- 01-ignite-feed/src/components/Post.jsx
+++ 01-ignite-feed/src/components/Post.jsx
@@ -21,20 +21,25 @@ export function Post() {
         >
           Públicado há 1h
         </time>
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-          <p> 👉 jane.design/doctorcare</p>
-          <p>
-            <a href="#">#novoprojeto </a> <a href="#">#nlw</a>{' '}
-            <a href="#">#rocketseat</a>
-          </p>
-        </div>
       </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p> 👉 jane.design/doctorcare</p>
+        <p>
+          <a href="#">#novoprojeto </a> <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe seu feedback" />
+        <button type="submit">Publicar</button>
+      </form>
     </article>
   )
 }
