15b0849e0eb356fbd0b97d649f7ece3d04b15238
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 <hugovarellaa@gmail.com>
2022-07-26 11:45:14 -0300

    Estilo App

--- 01-ignite-feed/src/App.jsx
+++ 01-ignite-feed/src/App.jsx
@@ -1,10 +1,21 @@
 import { Header } from './components/Header'
+
+import styles from './App.module.css'
 import './styles/global.css'
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.container}>
+        <aside>sidebar</aside>
+        <main>
+          <p>post</p>
+          <p>post</p>
+          <p>post</p>
+        </main>
+      </div>
     </div>
   )
 }
