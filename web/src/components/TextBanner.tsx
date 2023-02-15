d238756fafef99502c154e09215b67af69b7297b
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-14 23:16:22 -0300

    Delete web directory

--- web/src/components/TextBanner.tsx
+++
@@ -1,9 +0,0 @@
-export function TextBanner() {
-  return (
-    <h1 className="text-6xl text-white font-black mt-20">
-      Seu{" "}
-      <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span>{" "}
-      está aqui.
-    </h1>
-  );
-}
